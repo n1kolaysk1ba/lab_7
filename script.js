910decd11834fd293b4f45b6fad02512d7bd2965
N1kolay <73244584+n1kolaysk1ba@users.noreply.github.com>
2021-12-04 12:38:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function Generate(){
+    document.body.innerHTML = "<div class='circle' id ='circle' onclick='Question();'><div class='answer' id='answer'>Введіть своє питання!</div></div>"
+}
+Generate();
+function DeleteShaking(){
+    document.getElementById("circle").classList.remove('shaking');
+}
+function Question(){
+    var question='';
+    question = prompt("Напишіть ваше питання");
+    if (question.length==0) {
+        alert("Ви не ввели питання!")
+        return;
+    }
+    //Make ball shaking
+    document.getElementById("circle").classList.add('shaking');
+    setTimeout(DeleteShaking, 500);
+    answers = ["Yes", "No", "Maybe", "Probably"];
+    const randomElement = answers[Math.floor(Math.random() * answers.length)];
+    document.getElementById("answer").innerHTML = randomElement;
+}
